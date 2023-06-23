2eca698892254b5e260ad745c0a69451ef3fdcd8
pockocmoc <pockocmoc83@gmail.com>
2023-06-23 09:26:23 +0300

    0.1 version

--- src/Rocket.java
+++ src/Rocket.java
@@ -6,7 +6,7 @@ public class Rocket {
         Rocket rocket = new Rocket();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите слово для баннера на космодроме:");
+        System.out.println("Введите приветственное слово для баннера на космодроме\n(не более 25 символов):");
         String inputLine = scanner.nextLine();
         System.out.println("");
         System.out.println("");
@@ -16,15 +16,15 @@ public class Rocket {
 
     public void starShip(String bannerText) {
         System.out.println("   *       _            __________________________");
-        System.out.println("          / \\          |                          |");
-        System.out.println("**       /   \\         |" + addBanner(bannerText) + "|");
-        System.out.println("*       /     \\        |                          |");
-        System.out.println("        |     |        |__________________________|");
-        System.out.println("        |  *  |                                     ");
-        System.out.println("        |     |                       *            ");
+        System.out.println("          / \\          |    Стартовая площадка    |");
+        System.out.println("         /   \\         |                          |");
+        System.out.println("        /     \\   *    |" + addBanner(bannerText) + "|");
+        System.out.println("    *   |     |        |__________________________|");
+        System.out.println("        |  О  |                     |                ");
+        System.out.println("--------|     |---------------------|--------------");
         System.out.println("        |  c  |                                    ");
-        System.out.println("  *     |  о  |                                    ");
-        System.out.println("        |  ю  |       *                            ");
+        System.out.println("        |  о  |                                    ");
+        System.out.println("        |  ю  |                                    ");
         System.out.println("        |  з  |                                    ");
         System.out.println("        |_____|                                    ");
         System.out.println("       /_\\   /_\\                  ");
