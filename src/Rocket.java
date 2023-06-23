c4d05de15e9bfc0293bd5ec49afbac35d97c19b8
pockocmoc <pockocmoc83@gmail.com>
2023-06-23 07:14:03 +0300

    Initial commit

---
+++ src/Rocket.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Rocket {
+    public static void main(String[] args) {
+
+        Rocket rocket = new Rocket();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите слово для баннера на космодроме:");
+        String inputLine = scanner.nextLine();
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        rocket.starShip(inputLine);
+    }
+
+    public void starShip(String bannerText) {
+        System.out.println("   *       _            __________________________");
+        System.out.println("          / \\          |                          |");
+        System.out.println("**       /   \\         |" + addBanner(bannerText) + "|");
+        System.out.println("*       /     \\        |                          |");
+        System.out.println("        |     |        |__________________________|");
+        System.out.println("        |  *  |                                     ");
+        System.out.println("        |     |                       *            ");
+        System.out.println("        |  c  |                                    ");
+        System.out.println("  *     |  о  |                                    ");
+        System.out.println("        |  ю  |       *                            ");
+        System.out.println("        |  з  |                                    ");
+        System.out.println("        |_____|                                    ");
+        System.out.println("       /_\\   /_\\                  ");
+    }
+
+    private String addBanner(String text) {
+        // ограничиваем длину текста баннера до 27 символов
+        if (text.length() > 26) {
+            text = text.substring(0, 26);
+        }
+        // выравниваем текст по центру баннера
+        int spaces = (26 - text.length()) / 2;
+        String banner = "";
+        for (int i = 0; i < spaces; i++) {
+            banner += " ";
+        }
+        banner += text;
+        for (int i = spaces + text.length(); i < 26; i++) {
+            banner += " ";
+        }
+        return banner;
+    }
+}
